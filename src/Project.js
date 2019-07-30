c537e6a4b5ef4a89c2e968d23b3304886639146a
Matt Groberg <grobergmg@gmail.com>
2019-07-30 14:32:02 -0700

    display project details and change style

--- src/Project.js
+++ src/Project.js
@@ -3,32 +3,41 @@ import Moment from 'moment';
 
 function Project(props){
 	return (
-	<div className="jumbotron jumbotron-fluid">
-	  <div className="container">
-	    <h1 className="display-4">{props.project.name}</h1>
-	    {
-	    	props.onSignUp ? 
-	    	<button onClick={props.onSignUp} className="btn btn-danger">Sign Up</button> : null
-	    }
-	    
-	   	<ol>
-	    {
-	  		props.project.tasks.map((task,index)=>{
-	  			return (
-	  			<li key={index}>
-	  				{task.description}
-	  				<span
-	  				style={{marginLeft:'1rem'}}
-	  				 className="badge badge-secondary">
-	  					{Moment(task.date).format("MMM Do")}
-	  				</span>
-	  			</li>
-	  			)
-	  		})
-	  	}
-	    </ol>
-	  </div>
-	</div>
+	<div className="col-6">
+    <div className="card">
+    	<h5 className="card-header">{props.project.name}</h5>
+      <div className="card-body">
+        <h5 className="card-title">Host: {props.project.host.name}</h5>
+        <p className="card-text">Email: {props.project.host.email}</p>
+		    <button onClick={props.onSignUp} className="btn btn-danger">Sign Up</button>
+		    	<div>
+		    		<h5 className="card-text">Volunteers ({props.project.volunteers.length})</h5>
+		    		{
+		    			props.project.volunteers.map((volunteer,index)=>{
+		    				return <p key={index} className="card-text">{volunteer}</p>
+		    			})
+		    		}
+		    		<p className="card-text">To-do</p>
+				    <ol>
+					    {
+					  		props.project.tasks.map((task,index)=>{
+					  			return (
+					  			<li key={index}>
+					  				{task.description}
+					  				<span
+					  				style={{marginLeft:'1rem'}}
+					  				 className="badge badge-secondary">
+					  					{Moment(task.date).format("MMM Do")}
+					  				</span>
+					  			</li>
+					  			)
+					  		})
+					  	}
+			   		</ol>
+			   	</div>
+      </div>
+    </div>
+  </div>
 	)
 }
 
