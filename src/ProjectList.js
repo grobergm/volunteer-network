1165e19ed1ade61de8029ad0c11234d5bcc34114
Matt Groberg <grobergmg@gmail.com>
2019-07-30 14:32:45 -0700

    separate projects with tabs

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -60,11 +60,11 @@ class ProjectList extends Component{
 		})
 	}
 
-	handleSignUp(projectId){
+	handleSignUp(projectId,userName){
 		fetch(`http://localhost:8000/api/projects/signup/${this.props.profile._id}`,
 			{
 				method:'PUT',
-				body: JSON.stringify({projectId:projectId}),
+				body: JSON.stringify({projectId,userName}),
 				headers:{
 					'Content-Type': 'application/json',
 					'authorization': localStorage['token']
@@ -91,124 +91,136 @@ class ProjectList extends Component{
 				<div className="btn-group">
 					<button 
 						onClick={()=>{this.setState({view:'myProjects'})}} 
-						className="btn btn-primary">View My Projects
+						className="btn btn-primary">Hosting
 					</button>
 					<button 
-						onClick={()=>{this.setState({view:'search'})}} 
-						className="btn btn-secondary">Search For Projects
+						onClick={()=>{this.setState({view:'volProjects'})}} 
+						className="btn btn-secondary">Volunteering
 					</button>
 					<button 
 						onClick={()=>{this.setState({view:'newProject'})}} 
-						className="btn btn-success">Start New Project
+						className="btn btn-success">Start New
 					</button>
        	</div>
        	{
        		this.state.view==="myProjects" ?
-       			<div>
-       			<h2>Projects I'm involved in</h2>
+       			<div className="row" style={{marginTop:'1rem'}}>
 	       			{
 		       			this.props.profile.projects.map((project,index)=>{
-									return <Project project={project} key={index}/>
+		       				if (project.host._id===this.props.profile._id){
+		       					return <Project project={project} key={index}/>
+		       				}
 								})
 	       			}
        			</div>
 
        			: 
 
-       			this.state.view==="search" ?
-		       	<div>
-						  <div className="input-group mb-3">
-			       		<select 
-			       			id="selectFriendId" 
-			       			name="selectFriendId" 
-			       			className='form-control' 
-			       			onChange={this.handleChange}>
-			       			{
-			       				this.props.profile.friends.map((friend,index)=>{
-			       					return <option key={index} value={friend._id}>{friend.name}</option>
-			       				})
-			       			}
-			       		</select>
-			       		<div className="input-group-append">
-								  <button 
-								  	className='btn btn-primary' 
-								  	onClick={this.searchFriendProjects}>
-								  		See Projects
-								  </button>
-								</div>
-			       	</div>
-
-			       	{
-		       			this.state.friendProjects.map((project,index)=>{
-									return <Project 
-										onSignUp={()=>{this.handleSignUp(project._id)}} 
-										project={project} 
-										key={index}/>
+       			this.state.view==="volProjects" ?
+		       	
+		       	<div className="row" style={{marginTop:'1rem'}}>
+	       			{
+		       			this.props.profile.projects.map((project,index)=>{
+		       				if (project.host._id!==this.props.profile._id){
+		       					return <Project project={project} key={index}/>
+		       				}
 								})
 	       			}
-
-		       	</div>
+       			</div>
 
 						:
-
-						<div className="form-group">
-		       		<h2>New Project</h2>
-						  <label htmlFor="projectName">Name of Project</label>
-						  <input 
-						  	onChange={this.handleChange}
-						  	type="text" 
-						  	className="form-control" 
-						  	id="projectName" 
-						  	name="projectName"
-						  	aria-describedby="nameHelp" 
-						  	placeholder="Describe the project"
-						  	value={this.state.projectName}/>
-							<label htmlFor="task">Add Tasks To Project</label>
-						  <div className="input-group mb-3">
+						<div>
+							<div className="form-group">
+			       		<h2>New Project</h2>
+							  <label htmlFor="projectName">Name of Project</label>
 							  <input 
 							  	onChange={this.handleChange}
 							  	type="text" 
 							  	className="form-control" 
-							  	id="task" 
-							  	name="taskDescription"
-							  	aria-describedby="taskHelp" 
-							  	placeholder="Enter a Task"
-							  	value={this.state.taskDescription}/>
-							  <input 
-							  	onChange={this.handleChange}
-							  	type="date"
-							  	className="form-control"
-							  	id="taskDate"
-							  	name="taskDate"
-							  	value={this.state.taskDate}/>
-							  <div className="input-group-append">
-								  <button 
-								  	className='btn btn-success' 
-								  	onClick={this.addTask}>
-								  		Add Task
-								  </button>
+							  	id="projectName" 
+							  	name="projectName"
+							  	aria-describedby="nameHelp" 
+							  	placeholder="Describe the project"
+							  	value={this.state.projectName}/>
+								<label htmlFor="task">Add Tasks To Project</label>
+							  <div className="input-group mb-3">
+								  <input 
+								  	onChange={this.handleChange}
+								  	type="text" 
+								  	className="form-control" 
+								  	id="task" 
+								  	name="taskDescription"
+								  	aria-describedby="taskHelp" 
+								  	placeholder="Enter a Task"
+								  	value={this.state.taskDescription}/>
+								  <input 
+								  	onChange={this.handleChange}
+								  	type="date"
+								  	className="form-control"
+								  	id="taskDate"
+								  	name="taskDate"
+								  	value={this.state.taskDate}/>
+								  <div className="input-group-append">
+									  <button 
+									  	className='btn btn-success' 
+									  	onClick={this.addTask}>
+									  		Add Task
+									  </button>
+									</div>
 								</div>
+							  <ol>
+							  	{
+							  		this.state.tasks.map((task,index)=>{
+							  			return (
+							  			<li key={index}>
+							  				{task.description}
+							  				<span
+							  				style={{marginLeft:'1rem'}}
+							  				 className="badge badge-secondary">
+							  					{moment(task.date).format("MMM Do")}
+							  				</span>
+							  			</li>
+							  			)
+							  		})
+							  	}
+							  </ol>
+							  <button 
+									className="btn btn-primary" 
+									onClick={this.newProject}>Submit</button>
 							</div>
-						  <ol>
-						  	{
-						  		this.state.tasks.map((task,index)=>{
-						  			return (
-						  			<li key={index}>
-						  				{task.description}
-						  				<span
-						  				style={{marginLeft:'1rem'}}
-						  				 className="badge badge-secondary">
-						  					{moment(task.date).format("MMM Do")}
-						  				</span>
-						  			</li>
-						  			)
-						  		})
-						  	}
-						  </ol>
-						  <button 
-								className="btn btn-primary" 
-								onClick={this.newProject}>Submit</button>
-						</div>
+
+							<div>
+							  <div className="input-group mb-3">
+				       		<select 
+				       			id="selectFriendId" 
+				       			name="selectFriendId" 
+				       			className='form-control' 
+				       			onChange={this.handleChange}>
+				       			{
+				       				this.props.profile.friends.map((friend,index)=>{
+				       					return <option key={index} value={friend._id}>{friend.name}</option>
+				       				})
+				       			}
+				       		</select>
+				       		<div className="input-group-append">
+									  <button 
+									  	className='btn btn-primary' 
+									  	onClick={this.searchFriendProjects}>
+									  		See Projects
+									  </button>
+									</div>
+				       	</div>
+
+				       	{
+			       			this.state.friendProjects.map((project,index)=>{
+										return <Project 
+											onSignUp={()=>{this.handleSignUp(project._id,this.props.profile.name)}} 
+											project={project} 
+											key={index}/>
+									})
+		       			}
+		      		</div>
+		      	</div>
        	}
 		</div>
 		)
